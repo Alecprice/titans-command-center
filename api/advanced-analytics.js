b60454eaec2e3da1baa12faa0ec42ef2bdf6e47c
Alec Price <52548136+Alecprice@users.noreply.github.com>
2026-08-20 17:48:37 -0400

    Keep advanced analytics in single API gateway

--- api/advanced-analytics.js
+++
@@ -1,5 +0,0 @@
-import {advancedAnalyticsRoute} from '../src/advanced-analytics-api.mjs';
-
-export default async function handler(req,res){
-  return advancedAnalyticsRoute(req,res,process.env);
-}
