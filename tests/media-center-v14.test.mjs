9053a77a0b3cb47384c97cacf5807c5ed0a0cc17
Alec Price <52548136+Alecprice@users.noreply.github.com>
2026-08-21 14:12:48 -0400

    Make media PWA cache test version resilient

--- tests/media-center-v14.test.mjs
+++ tests/media-center-v14.test.mjs
@@ -10,7 +10,7 @@ test('Listen Watch is a first-class route and PWA asset',()=>{
   assert.match(html,/Listen \/ Watch/);
   assert.match(html,/media-center-v14\.css\?v=1/);
   assert.match(html,/media-center-v14\.js\?v=1/);
-  assert.match(sw,/titans-cc-brand-2026-v33/);
+  assert.match(sw,/const CACHE = 'titans-cc-brand-2026-v\d+'/);
   assert.match(sw,/\/media-center-v14\.css/);
   assert.match(sw,/\/media-center-v14\.js/);
 });
